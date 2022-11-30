d498e340ee5b9116ea5bd6ea2a02fac1dc22d286
andreaamaccio <115472186+andreaamaccio@users.noreply.github.com>
2022-11-30 15:39:33 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
 
+int funzione(int a) {
+    int b = a;
+    while (b>1) {
+        if (b==2)
+            return true;
+        else if (funzione(b-1) == true && a % (b-1) == 0)
+                return false;
+        else
+            b--;
+            }
+        }
