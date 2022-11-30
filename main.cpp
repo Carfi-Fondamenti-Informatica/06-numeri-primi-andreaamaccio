e97d01e7ea098da49e330b69ca94566836d902bf
andreaamaccio <115472186+andreaamaccio@users.noreply.github.com>
2022-11-30 16:54:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int numero;
     cin>>numero;
-    if (funzione(numero,2)==true)
+    if (funzione(numero,2)==1)
         cout<<"numero primo";
     else cout<<"numero non primo";
     return 0;
