c3215d178ae364bd1deac18f9c016fce076638c7
andreaamaccio <115472186+andreaamaccio@users.noreply.github.com>
2022-11-30 15:40:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int a;
+    cin>>a;
+    if(funzione(a)==true)
+        cout<<"numero primo";
+    else
+        cout<<"numero non primo";
+    return 0;
 }
+
